89412d095ffb993ad47f1876df966cd2c59f4192
HorusXenoverse <92953478+HorusXenoverse@users.noreply.github.com>
2022-10-24 12:37:18 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from http.client import ResponseNotReady
+import json
+from urllib import response
+from flask import Flask, render_template, url_for, request, jsonify
+from text_sentiment_prediction import *
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+@app.route('/predict-emotion', methods=["POST"])
+def predict_emotion():
+    # Obtener el texto ingresado del requerimiento POST.
+    input_text = request.json.get("text")
+
+    
+    if not input_text:
+        # Respuesta para enviar si input_text está indefinido.
+        response = {
+            "status" : "error",
+            "message" : "Por favor ingrese texto"
+        }
+        return jsonify(response)
+        
+        # Respuesta para enviar si input_text no está indefinido.
+    else:
+        predicted_emotion, predicted_emotion_img_url = predict(input_text)
+        response = {
+            "status" : "success",
+            "data" : {"predicted_emotion" : predicted_emotion,
+                     "predicted_emotion_img_url" : predicted_emotion_img_url}
+        }
+        return jsonify(response)
+
+
+        
+        # Enviar respuesta.         
+        
+       
+app.run(debug=True)
+
+
+
+    
